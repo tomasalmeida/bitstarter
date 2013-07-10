90e70fe6b834658ec32552e5c5ff75c4d9a4c36b
Tomas Dias Almeida <tomas.almeida@gmail.com>
2013-07-10 17:58:13 +0000

    Changed web.js (read from file)

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buffIndexFile =  fs.readFileSync("index.html");
+    response.send(buffIndexFile.toString('utf8'));
+    // old response, replaced
+    //response.send('Hello World 2!');
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
