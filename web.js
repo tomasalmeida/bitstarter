b216d1cf14344a4f02950ee3f6e7340190855d15
Tomas Dias Almeida <tomas.almeida@gmail.com>
2013-07-21 19:08:28 +0000

    server modified to bind 8080 port

--- web.js
+++ web.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-    var buffIndexFile =  fs.readFileSync("index.html");
+    var buffIndexFile =  fs.readFileSync(htmlfile);
     response.send(buffIndexFile.toString('utf8'));
-    // old response, replaced
-    //response.send('Hello World 2!');
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
